ea0f501ba63b1ca5cdb016666f3ee070fc099ba4
hellonayeon <hellonykwon@gmail.com>
2022-01-30 00:00:10 +0900

    feat: 연결리스트 만들기2 - 연속적인 데이터 삽입 (head가 첫번째 원소를 가리키는 경우)

--- 널널한 박교수의 C 강좌 고급편/linkedlist-create2.c
+++ 널널한 박교수의 C 강좌 고급편/linkedlist-create2.c
@@ -7,16 +7,17 @@ typedef struct ListNode {
     struct ListNode *link;
 } ListNode;
 
-void insert_node(ListNode **phead, ListNode *new_node);
+void insert_node(ListNode **phead, ListNode *p, ListNode *new_node);
 
 int main() {
-    ListNode *head = NULL;
+    ListNode *head = NULL, *prev_node = NULL;
     ListNode *node, *current_node;
 
     for(int i=1; i<=5; i++) {
         node = (ListNode *)malloc(sizeof(ListNode));
         node->data = i;
-        insert_node(&head, node);
+        insert_node(&head, prev_node, node);
+        prev_node = node;
     }
 
     current_node = head;
@@ -28,7 +29,19 @@ int main() {
     return 0;
 }
 
-void insert_node(ListNode **phead, ListNode *new_node) {
-    new_node->link = *phead;
-    *phead = new_node;
+void insert_node(ListNode **phead, ListNode *p, ListNode *new_node) {
+    printf("before : new_node = %p \n", new_node);
+    printf("before : *phead = %p \n", *phead);
+
+    if (p == NULL) {
+        new_node->link = *phead; // NULL로 초기화
+        *phead = new_node;
+    }
+    else {
+        new_node->link= p->link;
+        p->link = new_node;
+    }
+
+    printf("after : new_node = %p \n", new_node);
+    printf("after : *phead = %p \n", *phead);
 }
