005970676b6fe587b1c7958522a2c6d656aa458b
hellonayeon <hellonykwon@gmail.com>
2022-01-31 23:25:23 +0900

    [기초-조건/선택실행구조] 월 입력받아 계절 출력하기

---
+++ CodeUp C언어 기초 100제/1070.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+    scanf("%d", &month);
+
+    switch(month) {
+        case 12:
+        case 1:
+        case 2:
+            printf("%s", "winter");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            printf("%s", "spring");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            printf("%s", "summer");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            printf("%s", "fall");
+            break;
+    }
+}
