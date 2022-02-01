937bf15ad030506f713428b7e4d467b4677deeea
hellonayeon <hellonykwon@gmail.com>
2022-02-01 22:57:16 +0900

    [기초-종합] 함께 문제 푸는 날

---
+++ CodeUp C언어 기초 100제/1092.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    int day;
+    
+    scanf("%d %d %d", &a, &b, &c);
+    day=1;
+    while ( day%a != 0 || day%b != 0 || day%c != 0 ) {
+        day++;
+    }
+
+    printf("%d", day);
+}
