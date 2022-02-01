2101f1522ff39e773ef27ad8f9f6184b650a41c3
hellonayeon <hellonykwon@gmail.com>
2022-02-01 20:49:16 +0900

    [기초-반복실행구조] 0 입력될 때까지 무한 출력하기2

---
+++ CodeUp C언어 기초 100제/1073.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    while (1) {
+        scanf("%d", &n);
+        if (n == 0) {
+            break;
+        }
+        printf("%d\n", n);
+    }
+}
