b8689a2b405bde695cd1770f9da61e621b1eed81
hellonayeon <hellonykwon@gmail.com>
2022-02-01 23:17:36 +0900

    [기초-1차원배열] 이상한 출석 번호 부르기2

---
+++ CodeUp C언어 기초 100제/1094.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    int *arr;
+    int i;
+
+    scanf("%d", &n);
+
+    arr = (int *) malloc(n * sizeof(int));
+
+    for (i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i=n-1; i>=0; i--) {
+        printf("%d ", arr[i]);
+    }
+}
